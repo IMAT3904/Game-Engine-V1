ae8e51a73556b878066dcd064279e873ef64c8d1
Daniel Ball <me.danielball98@gmail.com>
2021-10-29 13:20:47 +0100

    Test10

--- Comit.bat
+++ Comit.bat
@@ -2,9 +2,10 @@
 cls
 echo [102;93m Github Commit [0m
 echo ^<Console^> [92mCommit to github starting...[0m
-set /p message= [91mEnter Message: 
+set /p message= [91mEnter Message:
+echo [92m
 git add .
 git commit -m "%message%"
-echo git push
+git push
 echo [92mCommit Finished!![0m
 pause
