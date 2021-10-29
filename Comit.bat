2678cc4d351b59feab6b673962c3e4f7cb398360
Daniel Ball <me.danielball98@gmail.com>
2021-10-29 13:17:25 +0100

    test5

--- Comit.bat
+++ Comit.bat
@@ -6,7 +6,7 @@ set /p message= [91mEnter Message:
 git add .
 git commit -m "%message%"
 SET c = git push
-echo c
+echo %c%
 if (c==true) echo [92mCommit Successful!![0m
 else echo [31mCommit Unsuccessful!![0m
 pause
