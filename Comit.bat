f40b823be18cb4349bd1ed895e52d74d058604a2
Daniel Ball <me.danielball98@gmail.com>
2021-10-29 13:16:49 +0100

    Test 3

--- Comit.bat
+++ Comit.bat
@@ -5,7 +5,7 @@ echo ^<Console^> [92mCommit to github starting...[0m
 set /p message= [91mEnter Message: 
 git add .
 git commit -m "%message%"
-c = git push 
+SET c = git push 
 if (c==true) echo [92mCommit Successful!![0m
 else echo [31mCommit Unsuccessful!![0m
 pause
